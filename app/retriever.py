8fb88e2f88362c5214362edf2e83bb652e6bbcc6
Maazin Khan <maazink@vailsys.com>
2026-08-07 22:35:52 -0500

    MMR - applied MMR to get diverse chunks and avoid redundant chunks

--- app/retriever.py
+++ app/retriever.py
@@ -1,7 +1,12 @@
 
 def create_retriever(vector_store):
     retriever = vector_store.as_retriever(
-        search_kwargs={"k": 4}
+        search_type="mmr",
+        search_kwargs={
+            "k": 4,
+            "fetch_k": 20,
+            "lambda_mult": 0.5
+        }
     )
 
     return retriever
